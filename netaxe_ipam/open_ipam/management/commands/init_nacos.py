45375bcbf73e0dec5908a0345afb332ba270ecf2
xhweng <xhweng@iflytek.com>
2022-12-06 15:19:25 +0800

    更新nacos

--- netaxe_ipam/open_ipam/management/commands/init_nacos.py
+++ netaxe_ipam/open_ipam/management/commands/init_nacos.py
@@ -8,6 +8,6 @@ nacosServer = nacos(ip=SERVER_IP, port=8848)
 nacosServer.registerService(
     serviceIp=SERVER_IP,  # 当前微服务ip、此时宿主机保持一致
     servicePort=SERVICE_PORT,  # 当前微服务暴露端口
-    serviceName="auth",
+    serviceName="ipam",
     groupName="default")
 nacosServer.healthyCheck()
